18c0b6fda100b22dc26aeb5a9cf15baebcb91226
Isaac Cerda <isaaccs2003@gmail.com>
2024-04-07 22:25:04 -0500

    dataBaseH2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (nombres, numero_cedula) VALUES ('Juan Perez', '123456789');
+INSERT INTO person (nombres, numero_cedula) VALUES ('Maria Rodriguez', '987654321');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE IF EXISTS person;
+CREATE TABLE person (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombres VARCHAR(255),
+    numero_cedula VARCHAR(255)
+);
